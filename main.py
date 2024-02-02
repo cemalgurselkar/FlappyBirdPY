c7c3dba03de348e55e71c448cdfacca3ae60c1e4
Cemal Gürsel Kar <116062524+cemalgurselkar@users.noreply.github.com>
2024-02-02 20:12:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,78 @@
+import configs
+import assets
+import pygame
+from object.background import Background
+from object.floor import Floor
+from object.column import Column
+from object.bird import Bird
+from object.gamestart_message import GameStartMessage
+from object.gameover_message import GameOverMessage
+from object.score import Score
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((configs.SCREEN_WIDTH,configs.SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+column_create_event = pygame.USEREVENT
+running = True
+gameover = False
+gamestarted = False
+
+
+assets.load_sprites()
+
+sprites = pygame.sprite.LayeredUpdates() 
+
+def create_sprites():
+    Background(0,sprites)
+    Background(1,sprites)
+    Floor(0,sprites)
+    Floor(1,sprites)
+
+    return Bird(sprites), GameStartMessage(sprites), Score(sprites)
+
+bird, game_start_message, score = create_sprites()
+
+#pygame.time.set_timer(column_create_event,1500)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == column_create_event:
+            Column(sprites)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and not gamestarted and not gameover:
+                gamestarted = True
+                game_start_message.kill()
+                pygame.time.set_timer(column_create_event,1500)
+            if event.key == pygame.K_SPACE and gameover:
+                gameover = False
+                gamestarted = False
+                sprites.empty()
+                bird, game_start_message, score = create_sprites()
+        
+        bird.handle_event(event)
+
+    screen.fill(0)
+    sprites.draw(screen)
+    
+    if gamestarted and not gameover:
+        sprites.update()
+
+    if bird.check_collison(sprites):
+        gameover = True
+        gamestarted = False
+        GameOverMessage(sprites)
+        pygame.time.set_timer(column_create_event,1500)
+
+    for sprite in sprites:
+        if type(sprite) is Column and sprite.is_passed():
+            score.value += 1
+
+
+    pygame.display.flip()
+    clock.tick(configs.FPS)
+
+pygame.quit() 
---
+++ object/bird.py
@@ -0,0 +1,43 @@
+import pygame.sprite
+import assets
+from layer import Layer
+import configs
+from object.column import Column
+from object.floor import Floor
+
+class Bird(pygame.sprite.Sprite):
+    def __init__(self,*groups):
+        self._layer = Layer.PLAYER
+        self.images = [assets.get_sprite("redbird-upflap"),
+                      assets.get_sprite("redbird-midflap"),
+                      assets.get_sprite("redbird-downflap")
+        ]
+        self.image =self.images[0]
+        self.rect = self.image.get_rect(topleft=(0,50))
+        
+        self.mask = pygame.mask.from_surface(self.image)
+        self.flap = 0
+
+        super().__init__(*groups)
+
+    def update(self):
+        self.images.insert(0,self.images.pop())
+        self.image = self.images[0]
+
+        self.flap += configs.GRAVITY
+        self.rect.y += self.flap
+
+        if self.rect.x < 50:
+            self.rect.x += 2
+
+    def handle_event(self,event):
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            self.flap = 0
+            self.flap -= 6
+
+    def check_collison(self,sprites):
+        for sprite in sprites:
+            if ((type(sprite) is Column or type(sprite) is Floor) and sprite.mask.overlap(self.mask, 
+                                                (self.rect.x - sprite.rect.x, self.rect.y - sprite.rect.y)) or self.rect.bottom < 0):
+                return True
+        return False
